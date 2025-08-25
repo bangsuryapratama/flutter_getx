a028ff281377e7640768beb3df86dc6925e039dc
Bangsur <bangsuryapratama@gmail.com>
2025-08-25 16:57:06 +0700

    Belajar GETX Flutter

---
+++ lib/app/modules/counter/controllers/counter_controller.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CounterController extends GetxController {
+  RxInt hitung = 1.obs; 
+
+  void increment() {
+    if (hitung < 100) {
+      hitung++;
+    } else {
+      Get.snackbar(
+        "Stop",
+        "Counter tidak boleh lebih dari 100",
+        backgroundColor: Colors.red,
+        icon: const Icon(Icons.warning, color: Colors.white),
+        colorText: Colors.white,
+      );
+    }
+  }
+
+
+  void decrement() {
+    if (hitung > 1) {
+      hitung--;
+    } else {
+      Get.snackbar(
+        "Stop",
+        "Counter tidak boleh kurang dari 1",
+        backgroundColor: Colors.red,
+        icon: const Icon(Icons.warning, color: Colors.white),
+        colorText: Colors.white,
+      );
+    }
+  }
+}
---
+++ lib/app/modules/counter/views/counter_view.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/counter_controller.dart';
+
+class CounterView extends GetView<CounterController> {
+  const CounterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CounterView'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Obx(() => AnimatedDefaultTextStyle(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                  style: TextStyle(
+                    fontSize: 16 + controller.hitung.value.toDouble(),
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                  ),
+                  child: Text("Hitungan ke: ${controller.hitung}"),
+                )),
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: controller.decrement,
+                  child: const Text(
+                    'MENCIUT',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: controller.increment,
+                  child: const Text(
+                    'MEMBADAG',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
